35664444c14a7afa8dd62e3cd5af4dbd74f79647
Katarzyna Bucka <katarzyna.bucka99@gmail.com>
2023-04-20 22:23:38 +0200

    Scaffolding WPF views

---
+++ BudgetTrackerWpf/Views/Common/UpdateModal.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BudgetTrackerWpf.Views.Common
+{
+    /// <summary>
+    /// Interaction logic for UpdateModal.xaml
+    /// </summary>
+    public partial class UpdateModal : UserControl
+    {
+        public UpdateModal()
+        {
+            InitializeComponent();
+        }
+    }
+}
